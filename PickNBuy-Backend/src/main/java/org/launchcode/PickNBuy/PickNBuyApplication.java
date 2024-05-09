bcd3d72b070ba95a964560240ff21c39fcc97453
priya <priyabalanusa@gmail.com>
2024-05-09 15:20:28 -0500

    first  commit

---
+++ PickNBuy-Backend/src/main/java/org/launchcode/PickNBuy/PickNBuyApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.PickNBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickNBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickNBuyApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
