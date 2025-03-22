209b30d661d84d25bc01abe29a049f2d8df33819
priya <priyabalanusa@gmail.com>
2025-03-21 20:20:48 -0500

    frontend created

---
+++ PickNBuy-Frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/layouts/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ PickNBuy-Frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div> <footer class="py-1">
+    <p class="text-center text-dark mt-1">
+      PickNBuy - 2025-2026, All Rights Reserved
+    </p>
+  </footer></div>
+  )
+}
+
+export default Footer
